197c4574c74029d0fd69cc294441d463cfe5cbfe
flash00 <dexlorraine@gmail.com>
2017-06-01 11:39:20 -0700

    add release 2 to hamsters.rb

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,15 +1,22 @@
 puts "What is the hamster's name?"
 input = gets.chomp
 puts "What is the hamster's volume level (from 1-10)?"
-vol = gets.chomp
+vol = gets.chomp.to_i
 puts "What is the hamster's fur color?"
 fur = gets.chomp
-puts "Is the hamster a good candidate for adoption?"
+puts "Is the hamster a good candidate for adoption (y/n)?"
 adopt = gets.chomp
+if adopt == "y"
+  adopt = true
+else
+  adopt = false
+end
 puts "What is the hamster's estimated age?"
 age = gets.chomp
 if age.length < 1
   age = nil
+else
+  age = age.to_i
 end
 #   on_a_budget = true
 # else
