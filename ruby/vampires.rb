e765a43b51c7ae08d0c39108a733d3372e3b4186
flash00 <dexlorraine@gmail.com>
2017-06-02 20:16:43 -0700

    driver code addes

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,12 +4,34 @@ name = gets.chomp
 
 puts "How old are you?"
 age = gets.chomp
+if age <100 && > 18
+  true
+else
+  false
 
 puts "What year were you born?"
 birthyear = gets.chomp
+if year is > 1900 && < 1999
+  true
+else
+  false
 
 puts "Our company cafeteria serves delicious garlic bread. Should we order some for you (y/n)?"
 gbread = gets.chomp
 
+if insurance = "y"
+  true
+elsif insurance = "n"
+  false
+else
+  puts "Please mark y or n."
+
 puts "Would you like to enroll in the company's health insurance (y/n)?"
 insurance = gets.chomp
+
+if insurance = "y"
+  true
+elsif insurance = "n"
+  false
+else
+  puts "Please mark y or n."
