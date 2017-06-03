bdff1634f15a5af56e3412bc3b0e374a316398e9
flash00 <dexlorraine@gmail.com>
2017-06-02 19:06:11 -0700

    Initial Commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+Driver code
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birthyear = gets.chomp
+
+puts "Our company cafeteria serves delicious garlic bread. Should we order some for you (y/n)?"
+gbread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance (y/n)?"
+insurance = gets.chomp
