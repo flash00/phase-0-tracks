b1f90e702de85a8961d83fdb26a58afc207b61f4
flash00 <dexlorraine@gmail.com>
2017-06-04 12:41:20 -0700

    R1&2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,67 +10,64 @@ puts "Welcome, " + name + "." #print to screen to confirm info entering program
 
 puts "How old are you?"
 age = gets.chomp.to_i #enters as integer so can compare with year remainder
-puts "Ok, " + age + " is a good age." #print to screen to confirm info entering program
+puts "Ok, " + age.to_s + " is a great age." #print to screen to confirm info entering program
 
 puts "What year were you born?"
 year = gets.chomp.to_i #change to integer to subtract from current year
-puts "Well, " + year + " was a very good year." #print to screen to confirm info entering program
+puts "Well, " + year.to_s + " was a very good year." #print to screen to confirm info entering program
 
-def check_age #this method will subtract reported year from current year to compare to reported age
+# subtract reported year from current year to compare to reported age
 real_age = 2017 - year
-if age == real_age
-  return true
-else
-  return false
-end
-p check_age
+  if age == real_age
+    real_age = true
+  else
+    real_age = false
+  end
+puts real_age
+
 
 #Vampires hate garlic. Do not even get a vampire started on garlic. Invite a vampire to an Italian restaurant even one time, and you’ll never hear the end of it.
 
 puts "Our company cafeteria serves delicious garlic bread. Should we order some for you (y/n)?"
-garlic_bread = gets.chomp.to_s
+g_bread = gets.chomp.to_s
 
-def garlic_bread
-if garlic_bread == "y"
-  return true
-elsif garlic_bread == "n"
-  return false
+if g_bread == "y"
+  garlic = true
+elsif g_bread == "n"
+  garlic = false
 else
   puts "Please mark y or n."
 end
-p garlic_bread
+puts garlic
+
 
 #Vampires are immortal, so they certainly don’t need health insurance.
 
 puts "Would you like to enroll in the company's health insurance (y/n)?"
 insurance = gets.chomp.to_s
 
-def wants_insurance
 if insurance == "y"
-  return true
+  e_insurance = true
 elsif insurance == "n"
-  return false
+  e_insurance = false
 else
   puts "Please mark y or n."
 end
-p wants_insurance
+puts e_insurance
+
 
 #Do the thing
-def condition_matching
-if
-  check_age != false && (garlic_bread == true || wants_insurance == true)
-  puts "Probably not a vampire."
-elsif
-  check_age != true && (garlic_bread == false || wants_insurance == false)
-  puts "Probably a vampire."
-elsif
-  check_age != true && garlic_bread == false && wants_insurance == false
-  puts "Almost certainly a vampire."
-elsif
-  name == "Drake Cula" || name == "Tu Fang"
-  puts "Definitely a vampire."
-else
-  puts "Results inconclusive."
-end
-p condition_matching
-end
+  if real_age != false && (garlic == true || e_insurance == true)
+    puts "Probably not a vampire."
+  elsif real_age != true && (garlic == false || e_insurance == false)
+    puts "Probably a vampire."
+  elsif real_age != true && garlic == false && e_insurance == false
+    puts "Almost certainly a vampire."
+  elsif
+    name == "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a vampire."
+  else
+    puts "Results inconclusive."
+  end
+
+
