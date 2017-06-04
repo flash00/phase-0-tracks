32cba4dfd88efa64b1589903a97119ea624e3393
flash00 <dexlorraine@gmail.com>
2017-06-04 10:29:27 -0700

    releases 1 & 2 intermediate passes

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,66 +1,70 @@
 #Driver code
 # Program to ask new employee:
-#Some known vampires are in the area, and we can check against the Werewolf Intelligence Bureau database for their aliases.
+#Some known vampires are in the area, and we can check names against the Werewolf Intelligence Bureau database for their aliases.
 
 puts "What is your name?"
-name = gets.chomp.capitalize
-puts "Welcome, " + name + "."
+name = gets.chomp.capitalize #entered info is capitalized as names usually are
+puts "Welcome, " + name + "." #print to screen to confirm info entering program
 
 # This is to try to trick the vampire, who is likely several hundreds of years old. If an employee gives an age and a year of birth that don’t line up mathematically, that employee might be a vampire.
 
 puts "How old are you?"
-age = gets.chomp.to_s
-puts "Ok, " + age + " is a good age."
+age = gets.chomp.to_i #enters as integer so can compare with year remainder
+puts "Ok, " + age + " is a good age." #print to screen to confirm info entering program
 
 puts "What year were you born?"
-year = gets.chomp.to_i
-puts "Well, " + year + " was a very good year."
+year = gets.chomp.to_i #change to integer to subtract from current year
+puts "Well, " + year + " was a very good year." #print to screen to confirm info entering program
 
-def check_age(#{year})
-  check = (2017 - #{year})
-  if #{age} == check
-    age == true
-  else
-    age == false
+def check_age #this method will subtract reported year from current year to compare to reported age
+real_age = 2017 - year
+if age == real_age
+  return true
+else
+  return false
 end
-puts check_age
+p check_age
 
 #Vampires hate garlic. Do not even get a vampire started on garlic. Invite a vampire to an Italian restaurant even one time, and you’ll never hear the end of it.
 
 puts "Our company cafeteria serves delicious garlic bread. Should we order some for you (y/n)?"
 garlic_bread = gets.chomp.to_s
 
+def garlic_bread
 if garlic_bread == "y"
-  true
+  return true
 elsif garlic_bread == "n"
-  false
+  return false
 else
   puts "Please mark y or n."
 end
+p garlic_bread
 
 #Vampires are immortal, so they certainly don’t need health insurance.
 
 puts "Would you like to enroll in the company's health insurance (y/n)?"
 insurance = gets.chomp.to_s
 
+def wants_insurance
 if insurance == "y"
-  true
+  return true
 elsif insurance == "n"
-  false
+  return false
 else
   puts "Please mark y or n."
 end
+p wants_insurance
 
 #Do the thing
-
+def condition_matching
 if
-  check_age != false && (garlic_bread == true || insurance == true)
+  check_age != false && (garlic_bread == true || wants_insurance == true)
   puts "Probably not a vampire."
 elsif
-  check_age == false && (garlic_bread == false || insurance == false)
+  check_age != true && (garlic_bread == false || wants_insurance == false)
   puts "Probably a vampire."
 elsif
-  check_age == false && garlic_bread == false && insurance == false
+  check_age != true && garlic_bread == false && wants_insurance == false
   puts "Almost certainly a vampire."
 elsif
   name == "Drake Cula" || name == "Tu Fang"
@@ -68,4 +72,5 @@ elsif
 else
   puts "Results inconclusive."
 end
+p condition_matching
 end
