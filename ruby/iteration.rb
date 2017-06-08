29741ef388738cce99c0cda9933f4a14c0384c7c
flash00 <dexlorraine@gmail.com>
2017-06-07 17:01:10 -0700

    adding release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,5 @@
+# Release 1 parts 1 and 2
+
 countries = [
 "China",
 "Denmark",
@@ -20,7 +22,7 @@ countries.each do |country|
   puts country
 end
 
-puts "After .each call:"
+puts "Countries: after .each call:"
 p countries
 
 ###############
@@ -33,12 +35,13 @@ numbers.map! do |number|
   puts number * 2
 end
 
-puts "After .map! call:"
+puts "Numbers: after .map! call:"
 p numbers
 
 ##################
+puts "Release 2 part 1 - array"
 
-num_array = [1, 2, 5, 7]
+num_array = [1, 3, 5, 7]
 
 num_array.select do |odd_num|
   if odd_num < 5
@@ -46,7 +49,24 @@ num_array.select do |odd_num|
   end
 end
 
-num_array = [1, 2, 5, 7]
+puts "Release 2 part 1 - hash"
+
+num_hash = {
+  1 => 'happy',
+  3 => 'sad',
+  5 => 'sad',
+  7 => 'happy'
+}
+
+num_hash.select do |number, status|
+  if number < 5
+    puts number
+  end
+end
+
+puts "Release 2 part 2 - array"
+
+num_array = [1, 3, 5, 7]
 
 num_array.reject do |odd_num|
   if odd_num == 5
@@ -54,26 +74,67 @@ num_array.reject do |odd_num|
   end
 end
 
-num_array = [1, 2, 5, 7]
+puts "Release 2 part 2 - hash"
 
-num_array.collect do |odd_num|
-  if odd_num == 5
+num_hash = {
+  1 => 'happy',
+  3 => 'sad',
+  5 => 'sad',
+  7 => 'happy'
+}
+
+num_hash.reject do |number, status|
+  if number == 5
+    puts number
+  end
+end
+
+puts "Release 2 part 3 - array"
+
+num_array = [1, 3, 5, 7]
+
+num_array.keep_if do |odd_num|
+  if odd_num == 3
     puts odd_num
   end
 end
 
-num_array = [1, 2, 5, 7]
+puts "Release 2 part 3 - hash"
 
-num_array.inject do |odd_num|
-  if odd_num == 5
+num_hash = {
+  1 => 'happy',
+  3 => 'sad',
+  5 => 'sad',
+  7 => 'happy'
+}
+
+num_hash.keep_if do |number, status|
+  if number == 3
+    puts number
+  end
+end
+
+puts "Release 2 part 4 - array"
+
+num_array = [1, 3, 5, 7]
+
+num_array.delete_if do |odd_num|
+  if odd_num < 5
     puts odd_num
   end
 end
 
-num_array = [1, 2, 5, 7]
+puts "Release 2 part 4 - hash"
 
-num_array.detect do |odd_num|
-  if odd_num == 5
-    puts odd_num
+num_hash = {
+  1 => 'happy',
+  3 => 'sad',
+  5 => 'sad',
+  7 => 'happy'
+}
+
+num_hash.delete_if do |number, status|
+  if number < 5
+    puts number
   end
 end
