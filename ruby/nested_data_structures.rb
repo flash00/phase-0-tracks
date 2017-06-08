8ac0c2f6aa0c5e9e062a7bbf46b757a888879f54
flash00 <dexlorraine@gmail.com>
2017-06-08 11:50:33 -0700

    Initial Commit

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,48 @@
+library = {
+
+  childrens:{
+    floor: 1,
+    num_books: 600,
+    most_popular:{
+      title: "The Snowy Day"
+      author: ["Ezra", "Jack", "Keats"]
+    },
+  }
+
+  fiction: {
+    floor: 2,
+    num_books: 1000,
+    most_popular:{
+      title: "The Catcher in the Rye"
+      author: ["J.". "D." "Salinger"]
+    },
+  },
+
+  computer_science:{
+    floor: 3,
+    num_books: 500,
+    most_popular:{
+      title: "Learning to Program"
+      author: ["Chris", "Pine"]
+    },
+  },
+
+  cooking:{
+    floor: 3,
+    num_books: 350,
+    most_popular:{
+      title: "The Joy of Cooking"
+      author: ["Irma", "S.", "Rombauer"]
+    },
+  }
+
+  gardening:{
+    floor: 3,
+    num_books: 150,
+    most_popular:{
+      title:
+      author:
+    },
+  }
+
+}
