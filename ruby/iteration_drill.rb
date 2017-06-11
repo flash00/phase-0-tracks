20ef8326539eacfeb74122ae0b450be25243c805
flash00 <dexlorraine@gmail.com>
2017-06-11 16:14:19 -0700

    continue work Sunday

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -5,21 +5,53 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 
 # 1. Iterate through the zombie_apocalypse_supplies array,
 # printing each item in the array separated by an asterisk
-# ----
+
+zombie_apocalypse_supplies.each {|x| print x, " * "}
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
 # ----
 
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+def alpha_sort(zombie_apocalypse_supplies)
+  index = 0
+  while index < zombie_apocalypse_supplies.length
+
+    if current_index
+    puts current_index
+index += 1
+
+  end
+
+  end
+
+
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
 # ----
+puts "Please enter item:"
+item = gets.chomp.to_s
+zombie_apocalypse_supplies.each do|supply|
+  if zombie_apocalypse_supplies.include? item
+    puts "Thanks, but we have #{item}."
+    break
+  else
+    puts "Thank you!"
+    break
+  end
+end
 
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
-# ----
+new_supplies = []
+counter = 0
+while counter < 5
+    new_supplies<<zombie_apocalypse_supplies[counter]
+    counter += 1
+end
+puts new_supplies
 
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
@@ -28,7 +60,9 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # documentation for Arrays.
 other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
-# ----
+
+unique_supplies = zombie_apocalypse_supplies | other_survivor_supplies
+puts unique_supplies
 
 # Hash Drills
 
@@ -44,17 +78,25 @@ extinct_animals = {
 
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
-# ----
+
+extinct_animals.each do|animal, year| print "#{animal}-#{year}*"
+end
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
-# ----
+
+extinct_animals.each do|animal, year| print "#{animal}-#{year}*" if year < 2000
+end
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
-# ----
+
+extinct_animals.each do|animal, year|
+  year = year + 3
+  print "#{animal}-#{year}*"
+end
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
@@ -62,7 +104,14 @@ extinct_animals = {
 # "Dodo"
 # "Saiga Antelope"
 # Do not use any special built-in methods.
-# ----
+def maybe_extinct(animal)
+  extinct_animals.each do|ex_animal|
+  if animal == extinct_animals[index]
+  return true
+end
+maybe_extinct("Andean Cat")
+maybe_extinct("Dodo")
+maybe_extinct("Saiga Antelope")
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
