4210a24d403a0d71b6383c917d68a87ce2b3c320
flash00 <dexlorraine@gmail.com>
2017-06-13 15:32:20 -0700

    designed a Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+=begin
+SPECIES -------------------------
+Friskius Catfrienemius
+
+CHARACTERISTICA -----------------
+tail: wagging
+nose: wet
+sound: bow wow
+color: varies
+legs: 4
+bestfriend: man/woman
+
+BEHAVIOR -------------------------
+run
+bark
+play
+sleep
+spin?
+fetch?
+shake?
+=end
+
