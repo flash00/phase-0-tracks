ba31348388cdc1ce9a191cf608cd122f35c91489
flash00 <dexlorraine@gmail.com>
2017-06-14 10:00:07 -0700

    add Automobile class with 2 instance methods, 50 times loop, data structure

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,7 @@
 class Puppy
+  def initialize
+    puts "initializing a new Puppy instance"
+  end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
@@ -31,3 +34,40 @@ fido.speak(3)
 fido.roll_over
 puts fido.dog_years(4)
 fido.spin
+
+
+
+class Automobiles
+
+  def initialize
+    puts "initializing new Automobile instance"
+  end
+
+
+
+  def back_to_the_future(year)
+    puts "you drove so fast you're in #{year}"
+  end
+
+  def change_tire
+    puts "congrats you changed the tire it only took #{rand(60)} minutes."
+  end
+
+end
+
+
+# camero = Automobiles.new
+# camero.back_to_the_future(1978)
+# camero.change_tire
+
+arr = []
+
+50.times do
+  arr << Automobiles.new
+end
+
+arr.each do |car|
+  car.back_to_the_future((rand(100) + 1900))
+  car.change_tire
+end
+
