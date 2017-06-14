961fd9592a7d5601ff728861e751353116e81d7d
flash00 <dexlorraine@gmail.com>
2017-06-14 09:19:21 -0700

    added driver code initializing instance of puppy

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,29 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    num.times{ puts "woof"
+    }
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(num)
+    num * 7
+  end
+
+  def spin
+    puts "**doggy spinning**"
+  end
 end
 
+
+fido = Puppy.new
+puts fido.fetch("ball")
+
+fido.speak(3)
+fido.roll_over
+puts fido.dog_years(4)
+fido.spin
