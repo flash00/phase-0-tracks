53be8a83839ae430e3136ddd15de30f226f6df52
flash00 <dexlorraine@gmail.com>
2017-06-16 22:48:03 -0700

    adding flight.rb and shout.rb

---
+++ ruby/shout.rb
@@ -0,0 +1,15 @@
+# module Shout
+
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     words + "!!!" + " :)"
+#   end
+
+# end
+
+# puts Shout.yell_happily("ice cream, pizza, puppydogs")
+# puts Shout.yell_angrily("taxes, homework, traffic")
+
