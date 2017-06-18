6693008ab6ce49c94e9ec0e27a92c2c63aacf19e
flash00 <dexlorraine@gmail.com>
2017-06-18 00:13:16 -0700

    added methods compare_guess get_length guess_limit and initialize

--- ruby/wordgame/word_game.rb
+++ ruby/wordgame/word_game.rb
@@ -1,4 +1,30 @@
 class Wordgame
+  def initialize(str)
+    @str = str
+  end
 
+  def get_length(str)
+    @str.length.to_i
+  end
+
+  def guess_limit(str)
+    @str.length.to_i * 2
+  end
+
+  def compare_guess(guess, str)
+    char = 0
+    result = ""
+    while char < @str.length
+      guess.each_char do
+        if guess[char] == @str[char]
+          result << guess[char]
+        else
+          result << "-"
+        end
+      char += 1
+      end
+    end
+    return result
+  end
 
 end
--- ruby/wordgame/word_game_spec.rb
+++ ruby/wordgame/word_game_spec.rb
@@ -1,10 +1,21 @@
 require_relative "word_game"
 
-describe Wordgame do |
-  let(:word) {Wordgame.new}
+describe Wordgame do
+  let(:word) {Wordgame.new("soda")}
 
   it "gets word length" do
-    expect(word.get_length).to eq word.length.to_i
+    word.get_length("soda")
+    expect(word.get_length("soda")).to eq 4
+  end
+
+  it "calculates guess limit" do
+    word.guess_limit("soda")
+    expect(word.guess_limit("soda")).to eq 8
+  end
+
+  it "compare guess to wordstring" do
+    word.compare_guess("s", "soda")
+    expect(word.compare_guess("s", "soda")).to eq "s---"
   end
 
 end
