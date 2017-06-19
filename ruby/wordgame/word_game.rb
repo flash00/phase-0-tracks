59062ba7c143076e56b3082f3c97ec99bb01b771
flash00 <dexlorraine@gmail.com>
2017-06-19 08:57:01 -0700

    added user interface

--- ruby/wordgame/word_game.rb
+++ ruby/wordgame/word_game.rb
@@ -1,17 +1,43 @@
+# Pseudocode for word-guessing game
+#all rspec tests passed prior to adding user interface
+
+#declare and initialize class
+
 class Wordgame
+  attr_accessor :guess
+  attr_accessor :str
+  attr_accessor :hint
+  attr_accessor :word_lth
   def initialize(str)
     @str = str
+    @guess = guess
     @guess_count = 0
     @limit = 0
+    @result = ""
+    @word_lth = word_lth
   end
+
+# step 1 get number based on length of entered string
 
   def get_length(str)
     @str.length.to_i
   end
 
+# step 2 show player how long word is
+
+  def show_word_length
+    @word_lth = @str.length.to_i
+     p "-" * @word_lth
+  end
+
+# step 3 number of guesses allowed is calculated by doubling the length of the entered string
+
   def guess_limit(str)
     @limit = @str.length.to_i * 2
   end
+
+# step 4 compare guess against entered string
+# step 5 if word not guessed print “You cannot defeat Word-Mojo!”
 
   def met_limit
     if @limit == @guess_count
@@ -20,22 +46,57 @@ class Wordgame
     end
   end
 
+# step 6 repeated guesses do not count against player
+
+def repeat_guess(guess, str)
+  if @guess != @str[-1]
+    print "Repeat. Please guess different letter."
+    return
+  end
+end
+
+# step 7 if characters of entered guess are in string include in result shown to player and use hyphens for missing letters
+# step 8 repeat steps 6 and 7 with successive guesses until either guesses allowed runs out or word is guessed
+
 def compare_guess(guess, str)
+#compare_guess stopped working when user interface added :(
   @guess_count = 0
   index = 0
-  result = ""
+  @result = ""
   while index < @str.length
-    guess.each_char do
-      if guess[index] == str[index]
-        result << guess[index]
+    @guess.each_char do
+      if @guess[index] == str[index]
+        @result << @guess[index]
       else
-        result << "-"
+        @result << "-"
       end
     @guess_count += 1
     index += 1
     end
   end
-  return result
+  return @result
+end
+
+# step 9 if word is guessed correctly print “You are a Word-Mojo Master!”
+
+def win_game
+  if @result == @str
+    print "You are a Word-Mojo Master!"
+  end
 end
 
 end
+
+#user interface
+
+p "Welcome to Word-Mojo!"
+p "Player 1, please enter a word."
+@str = gets.chomp
+word = Wordgame.new(@str)
+p word.show_word_length
+p "Player 2 enter a letter to see if it's in Player 1's word."
+@guess = gets.chomp
+p word.compare_guess(@guess, @str)
+p "Player 2 enter another letter."
+@guess = gets.chomp
+p word.compare_guess(@guess, @str)
--- ruby/wordgame/word_game_spec.rb
+++ ruby/wordgame/word_game_spec.rb
@@ -8,6 +8,11 @@ describe Wordgame do
     expect(word.get_length("soda")).to eq 4
   end
 
+  it "shows word length" do
+    word.show_word_length
+    expect(word.show_word_length).to eq "----"
+  end
+
   it "calculates guess limit" do
     word.guess_limit("soda")
     expect(word.guess_limit("soda")).to eq 8
@@ -18,9 +23,18 @@ describe Wordgame do
     expect(word.met_limit).to eq nil
   end
 
+  it "compares guess with previous guesses" do
+    word.met_limit
+    expect(word.met_limit).to eq nil
+  end
+
   it "compare guess to wordstring" do
     word.compare_guess("s", "soda")
     expect(word.compare_guess("s", "soda")).to eq "s---"
   end
 
+  it "word guessed" do
+    word.win_game
+    expect(word.win_game).to eq nil
+  end
 end
