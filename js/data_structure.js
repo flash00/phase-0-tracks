f6b1cb6563478794aaa0911e17b0f29ae9ac0204
flash00 <dexlorraine@gmail.com>
2017-06-23 15:59:07 -0700

    added 2 arrays and pushed them into single object

--- js/data_structure.js
+++ js/data_structure.js
@@ -1,1 +1,21 @@
-data stt
+var horseColors = ["blue", "pink", "glitter", "rainbow"];
+var horseNames = ["Journey", "Stormy", "Penelope", "Clifford"];
+
+console.log(horseColors);
+console.log(horseNames);
+
+horseColors.push("striped");
+horseNames.push("Spot");
+
+console.log(horseColors);
+console.log(horseNames);
+
+var horsies = {};
+
+console.log(horsies);
+
+for (var i = 0; i < horseColors.length; i++) {
+  horsies[horseNames[i]] = horseColors[i]
+};
+
+console.log(horsies);
