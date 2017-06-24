00a0d826af8ede64c496ad82a4e73716539d6424
flash00 <dexlorraine@gmail.com>
2017-06-23 17:15:22 -0700

    added research on looping and constructor functions

--- js/data_structure.js
+++ js/data_structure.js
@@ -23,7 +23,8 @@ console.log(horsies);
 function Car(color) {
   this.color = color;
   this.tireSpeed = Math.round(Math.random() * (60));
-  this.changeTire = function() { console.log("Congratulations, the " + this.color + " car is ready. We changed your tire in  " + this.tireSpeed + " minutes."); };
+  this.changeTire = function() { console.log("Congratulations, the " + this.color + " car is ready. We changed your tire in  " + this.tireSpeed + " minutes.");
+  };
 };
 
 var firstCar = new Car("blue");
@@ -33,3 +34,8 @@ var secondCar = new Car("red");
 secondCar.changeTire();
 var thirdCar = new Car("green");
 thirdCar.changeTire();
+
+// Release 3:
+// If you wanted to loop through the keys and values of an object, how would you do that? By using a for loop, while loop or .forEach. One drawback is that the properties in a JavaScript object are not sorted, i.e. have no order.
+
+// Are there advantages to using constructor functions to create objects? Takes less code and is clear. Disadvantages? Could be an unneccessary function call and you can't simultaneously add properties like the object literal.
