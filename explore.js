caf5820d2cc14f16a6f039fe2d9c2f140f198fd8
flash00 <dexlorraine@gmail.com>
2017-06-25 19:01:27 -0700

    initial commit

---
+++ explore.js
@@ -0,0 +1,13 @@
+// Create a function revString that accepts a string. Splits the string into letters. Loops through each letter in the string and reverses its place. Joins the letters into a new string. Returning the string with letters in reverse order.
+
+var newWord = [];
+
+function revString(str){
+  var ltrStr = str.split("");
+  for (var i = ltrStr.length -1; i >= 0; i -=1){
+    newWord.push(ltrStr[i]);
+  }
+    return newWord.join("");
+}
+
+console.log(revString("baloon"));
