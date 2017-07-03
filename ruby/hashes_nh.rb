6891b2f8e51b156002c02fbe15fa0e58ab9fed1d
flash00 <dexlorraine@gmail.com>
2017-07-02 20:06:38 -0700

    revised adding case statement for value revisions to be added in proper form (integer, boolean or string) and cleaned up a little

--- ruby/hashes_nh.rb
+++ ruby/hashes_nh.rb
@@ -1,61 +1,63 @@
-#Release 2 Use a Hash in a Program
-
-# # create hash with client name, age, number of children, decor theme, color preferences, dislikes
-
-
+# This is my hash for a rabbit client named Hopsy.
 client_info = {
-  name: "Fluffy",
-  age:"2",
-  children: "12",
-  decor_theme: "carrots",
+  name: "Hopsy Hopsalot",
+  age: 2,
+  children: 12,
+  decor_theme: "meadow",
   color_preferences: "green",
   dislikes: "foxes",
-  onsite: true
+  local: true,
+  garage: false,
+  pool: false
 }
 
+# Might be cleaner to initiate an empty hash instead of overwriting Hopsy's.
+# client_info = Hash.new
 
-# Prompt the designer/user for all of this information.
+# Prompt the designer for client information. A mixture of strings, integers and boolians
 puts "Please enter client's name."
-client_info[:name] = gets.chomp
+client_info[:name] = gets.chomp.to_s
 puts "Please enter client's age."
-client_info[:age] = gets.chomp
-puts "Please enter how many children client has."
-client_info[:children] = gets.chomp
+client_info[:age] = gets.chomp.to_i
+puts "Please enter the number of children client has."
+client_info[:children] = gets.chomp.to_i
 puts "Please enter client's decor theme."
-client_info[:decor_theme] = gets.chomp
+client_info[:decor_theme] = gets.chomp.to_s
 puts "Please enter client's color preferences."
-client_info[:color_preferences] = gets.chomp
+client_info[:color_preferences] = gets.chomp.to_s
 puts "Please enter client's strong dislikes."
-client_info[:dislikes] = gets.chomp
-puts "The client is onsite? (true/false)"
+client_info[:dislikes] = gets.chomp.to_s
+puts "The client is local? (true/false)"
+client_info[:onsite] = gets.chomp
+puts "The client has garage? (true/false)"
+client_info[:onsite] = gets.chomp
+puts "The client has pool? (true/false)"
 client_info[:onsite] = gets.chomp
 
-# # Convert any user input to the appropriate data type.
-client_info.each do |key, value|
-  puts value.to_s
-end
-
-
-
-# # Print the hash back out to the screen when the designer has answered all of the questions.
+# Print hash for the designer to review.
 p client_info
 
-# # Give the user the opportunity to update a key (no need to loop, once is fine). After all, sometimes users make mistakes! If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+# Designer has opportunity to update any key. Typing "none" will take designer out of review and print hash.
 
-puts "Please review and enter key if you would like to update. If no updates please type 'none'."
+puts "Please review and enter a key if you would like to update it. If not, type 'none'."
 
 key_update = gets.chomp
-
   if key_update == 'none'
     p client_info
   else
-    puts "Please enter value."
+    puts "Enter value."
     value = gets.chomp
-    client_info[key_update.to_sym] = value.to_s
+    case
+      when "name", "decor_theme", "color_preferences", "dislikes"
+        client_info[key_update.to_sym] = value.to_s
+      when "age", "children"
+        client_info[key_update.to_sym] = value.to_i
+      else
+        client_info[key_update.to_sym] = value
+      end
   end
 
+# Once designer has finished updating key and value information the revised hash prints.
 
-
-# # Print the latest version of the hash, and exit the program.
 p client_info
 
