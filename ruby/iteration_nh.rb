ffc0994a8753fdd95a2869ed2988fa1d078ea362
flash00 <dexlorraine@gmail.com>
2017-07-03 15:35:44 -0700

    added iteration through hash using .map

--- ruby/iteration_nh.rb
+++ ruby/iteration_nh.rb
@@ -1,21 +1,21 @@
-# my methods that take a block
+# # my methods that take a block
 
-# def first_block
-#   puts "Block hasn't run yet..."
-#   yield
-#   puts "Now it has."
-# end
+def first_block
+  puts "Block hasn't run yet..."
+  yield
+  puts "Now it has."
+end
 
-# first_block {puts "This is the block."}
+first_block {puts "This is the block."}
 
-# def dessert_block
-#   puts "Dessert is the best!"
-#   yield("ice cream", "cake")
-# end
+def dessert_block
+  puts "Dessert is the best!"
+  yield("ice cream", "cake")
+end
 
-# dessert_block {|dessert1, dessert2| puts "And the best desserts are #{dessert1} and #{dessert2}!"}
+dessert_block {|dessert1, dessert2| puts "And the best desserts are #{dessert1} and #{dessert2}!"}
 
-# # Release 1 part 1
+# Release 1 part 1
 
 countries = [
 'Laos',
@@ -31,139 +31,81 @@ country_capitals = {
   'Nigeria' => 'Abuja'
 }
 
-# puts "Release 1 part 2"
+puts "Release 1 part 2"
 
-# puts "printing 'countries' array before .each call:"
-# puts countries
+puts "printing 'countries' array before .each call:"
+puts countries
 
+#iterate through the array with .each
 
-# #iterate through the array with .each
-# countries.each do |country|
-#   p "calling .each on #{country}"
-# end
+countries.each do |country|
+  p "calling .each on #{country}"
+end
 
-# puts "printing 'countries' array after .each call:"
-# p countries
-# puts #blank line
-# puts #blank line
+puts "printing 'countries' array after .each call:"
+p countries
+puts #blank line
+puts #blank line
 
-# puts "printing 'countries' array before .map! call:"
-# puts countries
-# #iterate through the array with .map!
-# countries.map! do |country|
-#   country.reverse
-# end
+puts "printing 'countries' array before .map! call:"
+puts countries
 
-# puts "printing 'countries' array after .map! call:"
-# p countries
+#iterate through the array with .map!
 
-# puts "printing the hash 'country_capitals' before .each call:"
-# puts country_capitals
-# #iterate through the hash with .each
-# country_capitals.each do |country, capital|
-#   puts "Calling .each on #{capital} the capital of #{country}."
-# end
+countries.map! do |country|
+  country.reverse
+end
 
-# puts "printing the hash 'country_capitals' after .each call:"
-# p country_capitals
+puts "printing 'countries' array after .map! call:"
+p countries
 
-# ##################
-# puts "Release 2 part 1 - array"
+puts "printing the hash 'country_capitals' before .each call:"
+puts country_capitals
 
-# num_array = [1, 3, 5, 7]
+#iterate through the hash with .each
 
-# num_array.select do |odd_num|
-#   if odd_num < 5
-#     puts odd_num
-#   end
-# end
+country_capitals.each do |country, capital|
+  puts "Calling .each on #{capital} the capital of #{country}."
+end
 
-# puts "Release 2 part 1 - hash"
+puts "printing the hash 'country_capitals' after .each call:"
+p country_capitals
 
-# num_hash = {
-#   1 => 'happy',
-#   3 => 'sad',
-#   5 => 'sad',
-#   7 => 'happy'
-# }
+puts "printing the hash 'country_capitals' before .map call:"
+puts country_capitals
 
-# num_hash.select do |number, status|
-#   if number < 5
-#     puts number
-#   end
-# end
+#iterate through the hash with .map
 
-# puts "Release 2 part 2 - array"
+invert_capitals = country_capitals.map do |country, capital|
+  country_capitals.invert
+end
+puts #blank line
+puts "Here is the .map modified hash stored in a new variable."
+p invert_capitals
+puts #blank line
 
-# num_array = [1, 3, 5, 7]
+puts "printing the hash 'country_capitals' after .map call:"
+p country_capitals
 
-# num_array.reject do |odd_num|
-#   if odd_num == 5
-#     puts odd_num
-#   end
-# end
+puts "Release 2: Array"
 
-# puts "Release 2 part 2 - hash"
+odd_nums = [1, 3, 5, 7, 9]
 
-# num_hash = {
-#   1 => 'happy',
-#   3 => 'sad',
-#   5 => 'sad',
-#   7 => 'happy'
-# }
+# p odd_nums.delete_if {|num| num % 3 == 0}
+p odd_nums.keep_if {|num| num % 3 == 0}
+p odd_nums.select {|num| num % 3 == 0}
+p odd_nums.drop_while {|num| num < 7}
 
-# num_hash.reject do |number, status|
-#   if number == 5
-#     puts number
-#   end
-# end
+puts "Release 2: Hash"
 
-# puts "Release 2 part 3 - array"
+hundreds = {
+  a: 100,
+  b: 200,
+  c: 300,
+  d: 400
+}
 
-# num_array = [1, 3, 5, 7]
-
-# num_array.keep_if do |odd_num|
-#   if odd_num == 3
-#     puts odd_num
-#   end
-# end
-
-# puts "Release 2 part 3 - hash"
-
-# num_hash = {
-#   1 => 'happy',
-#   3 => 'sad',
-#   5 => 'sad',
-#   7 => 'happy'
-# }
-
-# num_hash.keep_if do |number, status|
-#   if number == 3
-#     puts number
-#   end
-# end
-
-# puts "Release 2 part 4 - array"
-
-# num_array = [1, 3, 5, 7]
-
-# num_array.delete_if do |odd_num|
-#   if odd_num < 5
-#     puts odd_num
-#   end
-# end
-
-# puts "Release 2 part 4 - hash"
-
-# num_hash = {
-#   1 => 'happy',
-#   3 => 'sad',
-#   5 => 'sad',
-#   7 => 'happy'
-# }
-
-# num_hash.delete_if do |number, status|
-#   if number < 5
-#     puts number
-#   end
-# end
+# p hundreds.delete_if {|letter, number| number <= 200}
+# p hundreds.keep_if {|letter, number| number <= 200}
+# p hundreds.select {|letter, number| number <= 200}
+p hundreds.reject {|letter, number| number <= 200}
