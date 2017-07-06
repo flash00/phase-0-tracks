5010778b92d0cab08c770f7deb58959885b4d836
flash00 <dexlorraine@gmail.com>
2017-07-05 17:06:13 -0700

    added and tested fibonacci method

--- ruby/problem_solving_methods_nh.rb
+++ ruby/problem_solving_methods_nh.rb
@@ -1,12 +1,30 @@
-def search_array(array, integer)
-  new_array = []
-    array.each {|num| new_array << num
-      if num == integer
-       p new_array.length-1
-      end
-    }
-    p nil
+# def search_array(array, integer)
+#   new_array = []
+#     array.each {|num| new_array << num
+#       if num == integer
+#        p new_array.length-1
+#       end
+#     }
+#     p nil
+# end
+
+# arr = [42, 89, 23, 1]
+# search_array(arr, 42)
+
+def fib(num)
+  fibis = [0, 1]
+  num1 = 0
+  num2 = 1
+  count = 3
+  while count <= num
+    fib_current = num1 + num2
+    fibis << fib_current
+    num1 = num2
+    num2 = fib_current
+    count += 1
+  end
+  p fibis
 end
 
-arr = [42, 89, 23, 1]
-search_array(arr, 42)
+  fib(100)
+
