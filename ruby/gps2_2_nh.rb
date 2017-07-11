9e591109f4e5a59e7faf887487d1a85b2dcb6d8f
flash00 <dexlorraine@gmail.com>
2017-07-11 16:49:59 -0700

    added pseudocode

--- ruby/gps2_2_nh.rb
+++ ruby/gps2_2_nh.rb
@@ -1,27 +1,28 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps:
-  # [fill in any steps here]
+  # split input string into array
+  # push array elements into hash
   # set default quantity
   # print the list to the console [can you use one of your other methods here?]
-# output: [what data type goes here, array or hash?]
+# output: hash
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
-# steps:
-# output:
+# steps: push item name to list with quantity
+# output: new list with updated item quantity
 
 # Method to remove an item from the list
-# input:
-# steps:
-# output:
+# input: list, item name to remove
+# steps: delete item from list
+# output: new list with item deleted
 
 # Method to update the quantity of an item
-# input:
-# steps:
-# output:
+# input: list, item, new quantity
+# steps: set new quantity for item in list
+# output: new list with updated quantity for item
 
 # Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
+# input: our list with item names and quantities
+# steps: print out a sentence for each item with its quantity
+# output: list of sentences with item name and quantity
