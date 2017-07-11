7478c19e4f29607ebfc4a4c046a019468c9a14f4
flash00 <dexlorraine@gmail.com>
2017-07-10 19:52:42 -0700

    3 attribute-changing methods and 2 getter methods added to Santa class

--- ruby/santa_nh.rb
+++ ruby/santa_nh.rb
@@ -1,35 +1,66 @@
 class Santa
 
-def initialize(gender, ethnicity)
-  puts "Initializing Santa instance..."
-  reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-  @age = 0
-  @gender = gender
-  @ethnicity = ethnicity
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance..."
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+    @gender = gender
+    @ethnicity = ethnicity
+  end
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}!"
+  end
+
+  def age
+    @age
+  end
+
+  # def age=(new_age)
+  #   @age = new_age
+  # end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  def celebrate_birthday
+   puts "Santa is #{@age + 1}"
+  end
+
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    puts @reindeer_ranking.push(reindeer)
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+    puts @gender
+  end
 
 end
 
-def speak
-  puts "Ho, ho, ho! Haaaappy holidays!"
-end
-
-def eat_milk_and_cookies(cookie)
-  puts "That was a good #{cookie}!"
-end
-
-end
-
-# kris_kringle = Santa.new("opt-out", "magical")
+kris_kringle = Santa.new("opt-out", "magical")
 # kris_kringle.speak
 # kris_kringle.eat_milk_and_cookies("snickerdoodle")
 
-santas = []
-genders = ["opt-out", "undeclared", "male", "female", "bigender", "n/a"]
-ethnicities = ["magical", "mystical", "non-magical", "non-mystical", "northpole-ish", "southpole-ish"]
+# santas = []
+# genders = ["opt-out", "undeclared", "male", "female", "bigender", "n/a"]
+# ethnicities = ["magical", "mystical", "non-magical", "non-mystical", "northpole-ish", "southpole-ish"]
 
-genders.length.times do |i|
-  santas << Santa.new(genders[i], ethnicities[i])
-end
+# genders.length.times do |i|
+#   santas << Santa.new(genders[i], ethnicities[i])
+# end
 
-puts "Diverse Initializations:"
-print santas
+# puts "Diverse Initializations:"
+# print santas
+
+kris_kringle.celebrate_birthday
+kris_kringle.get_mad_at("Vixen")
+kris_kringle.gender=("different")
+p kris_kringle.age
+p kris_kringle.ethnicity
