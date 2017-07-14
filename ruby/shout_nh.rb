0d2a5f321e6f5434c31f68a4224f77b40363fcd7
flash00 <dexlorraine@gmail.com>
2017-07-14 16:15:04 -0700

    initial commit

---
+++ ruby/shout_nh.rb
@@ -0,0 +1,32 @@
+# module Shout
+
+# end
+
+# puts Shout.yell_happily("ice cream, pizza, cookies")
+# puts Shout.yell_angrily("taxes, homework, traffic")
+
+module Shout
+  def yell_angrily(words)
+    p words + "!!!" + " :("
+  end
+
+  def yell_happily(words)
+    p words + "!!!" + " :)"
+  end
+end
+
+class Angry_otter
+  include Shout
+end
+
+class Angry_chihuahua
+  include Shout
+end
+
+angry_otter = Angry_otter.new
+angry_otter.yell_angrily("We hate sharks")
+angry_otter.yell_happily("We love abolone and crabs")
+
+angry_chihuahua = Angry_chihuahua.new
+angry_chihuahua.yell_angrily("We hate big dogs")
+angry_chihuahua.yell_happily("We love walkies")
