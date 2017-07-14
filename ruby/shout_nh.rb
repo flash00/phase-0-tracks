bf494b04ec22b6e0243c54d1abb30d1c94e8ca68
flash00 <dexlorraine@gmail.com>
2017-07-14 16:41:50 -0700

    converted standalone module to a mixin

--- ruby/shout_nh.rb
+++ ruby/shout_nh.rb
@@ -1,40 +1,40 @@
-module Shout
-  def self.yell_angrily(words)
-    words + ". Bah!!!" + " :("
-  end
-end
-
-module Shout
-  def self.yelling_happily(words)
-    words + ". Hooray!!!" + " :)"
-  end
-end
-
-puts Shout.yelling_happily("Ice cream, pizza, cookies")
-puts Shout.yell_angrily("Taxes, pollution, traffic")
+# module Shout
+#   def self.yell_angrily(words)
+#     words + " Bah!!!" + " :("
+#   end
+# end
 
 # module Shout
-#   def yell_angrily(words)
-#     p words + "!!!" + " :("
-#   end
-
-#   def yell_happily(words)
-#     p words + "!!!" + " :)"
+#   def self.yelling_happily(words)
+#     words + " Hooray!!!" + " :)"
 #   end
 # end
 
-# class Angry_otter
-#   include Shout
-# end
+# puts Shout.yelling_happily("Ice cream, pizza, cookies.")
+# puts Shout.yell_angrily("Taxes, pollution, traffic.")
 
-# class Angry_chihuahua
-#   include Shout
-# end
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
 
-# angry_otter = Angry_otter.new
-# angry_otter.yell_angrily("We hate sharks")
-# angry_otter.yell_happily("We love abolone and crabs")
+  def yelling_happily(words)
+    words + "!!!" + " :)"
+  end
+end
 
-# angry_chihuahua = Angry_chihuahua.new
-# angry_chihuahua.yell_angrily("We hate big dogs")
-# angry_chihuahua.yell_happily("We love walkies")
+class Angry_Otter
+  include Shout
+end
+
+class Angry_Chihuahua
+  include Shout
+end
+
+angry_otter = Angry_Otter.new
+puts angry_otter.yell_angrily("We hate sharks. Boo-hoo")
+puts angry_otter.yelling_happily("We love abolone. Yay")
+
+angry_chihuahua = Angry_Chihuahua.new
+puts angry_chihuahua.yell_angrily("We hate big dogs. Ick")
+puts angry_chihuahua.yelling_happily("We love walkies. Yay")
