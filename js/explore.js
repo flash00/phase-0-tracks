8338f43fa5b485647c00f6cfa76bb72699a534be
flash00 <dexlorraine@gmail.com>
2017-07-18 21:59:23 -0700

    wrote JavaScript program with function reverseString, tested with drivercode and printed string variable using boolean logic

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,15 @@
-Create function that reverses a string.
+// Create function that reverses a string.
+// input - string
+// loop through each letter of the string backwards
+// save each letter to a data structure as you loop
+// output - string with letters reversed
+var reverseStr = "";
+function reverseString(str){
+  for (var i = str.length-1; i >= 0; i -= 1) {
+    reverseStr += str[i];
+  };
+  return reverseStr;
+}
+
+console.log(reverseString("hello"));
+if (1 == 1) console.log(reverseStr);
