c6d1e9bb9882a4784a74018077b8d59499957afa
flash00 <dexlorraine@gmail.com>
2017-07-19 13:52:19 -0700

    wrote constructor function for car with model color and year attributes and initialized 3 cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,5 +15,43 @@ function toObject(namesArray, valuesArray){
   return horses;
 }
 
-console.log(toObject(names, colors));
+// console.log(toObject(names, colors));
 
+// *************************
+
+var car = {};
+
+function Car(model, color, year) {
+
+console.log("Our new car:", this);
+
+this.model = model;
+this.color = color;
+this.year = year;
+
+this.speed = function(speed) { console.log(speed + " miles per hour");};
+
+
+console.log("CAR INITIALIZATION COMPLETE");
+}
+
+console.log("Let's build a car...");
+var anotherCar = new Car("Camero", "golden", 1972);
+console.log(anotherCar);
+console.log("Our car can go fast: ");
+anotherCar.speed(200);
+console.log("---------");
+
+console.log("Let's build another car...");
+var secondCar = new Car("Pinto", "lime green", 1976);
+console.log(secondCar);
+console.log("Our car can go fast: ");
+anotherCar.speed(45);
+console.log("---------");
+
+console.log("Let's build another car...");
+var thirdCar = new Car("Shelby Cobra", "blue with white racing stripes", 1969);
+console.log(thirdCar);
+console.log("Our car can go fast: ");
+anotherCar.speed(180);
+console.log("---------");
