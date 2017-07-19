726deb2f2685fe7fd46a727f581015cbab07f4b1
flash00 <dexlorraine@gmail.com>
2017-07-19 13:16:51 -0700

    wrote function to combine two arrays into an object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,8 +1,19 @@
+var names = ["Ed", "Journey", "Percy", "Marty"];
 var colors = ["blue", "pink", "sparkle", "purple"];
-var names = ["Ed", "Journey", "Percy", "Mr. Go"];
 
 names.push("Billy");
 colors.push("Banana");
 
-console.log(names);
-console.log(colors);
+// console.log(names);
+// console.log(colors);
+
+
+function toObject(namesArray, valuesArray){
+  var horses = {};
+  for(var i = 0; i < names.length; i += 1)
+    horses[names[i]] = colors[i];
+  return horses;
+}
+
+console.log(toObject(names, colors));
+
