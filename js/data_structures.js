4d46cd2e5b82177049931cc14fd18a4b25bbf79e
flash00 <dexlorraine@gmail.com>
2017-07-19 12:11:55 -0700

    created 2 arrays names and colors and pushed items into each

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "pink", "sparkle", "purple"];
+var names = ["Ed", "Journey", "Percy", "Mr. Go"];
+
+names.push("Billy");
+colors.push("Banana");
+
+console.log(names);
+console.log(colors);
