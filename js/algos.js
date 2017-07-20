81eaac5622af3fa08bf2a9caf9a646960e4f82b0
flash00 <dexlorraine@gmail.com>
2017-07-19 17:41:26 -0700

    wrote longestWord (or phrase) function and tested with driver code

--- js/algos.js
+++ js/algos.js
@@ -4,11 +4,20 @@ write function longestWord which loops through the list comparing the length of 
 output: longest word in list */
 
 function longestWord(wordList) {
-  for (var i = 0; i <= wordList.length; i += 1)
+  for (var i = 0; i <= wordList.length; i += 1) {
     if (wordList[i].length < wordList[i + 1].length) {
       wordList[i] = wordList[i + 1];
-      return wordList[i];
+      if (wordList[wordList.length-1].length > wordList[i].length) {
+        wordList[i] = wordList[wordList.length-1]
+      }
+    return wordList[i];
     }
+  }
 }
 
-console.log(longestWord());
+var petsArray = ["kitty", "bunny", "turtle", "slug"]
+var colorsArray = ["blue", "red", "pink", "orange"]
+var phrasesArray = ["raindrops on roses", "whiskers on kittens", "bright copper kettles", "warm woolen mittens"]
+console.log(longestWord(colorsArray));
+console.log(longestWord(petsArray));
+console.log(longestWord(phrasesArray));
