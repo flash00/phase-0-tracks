2f8196e07b801043e7996375dbb5fca3c973aa51
flash00 <dexlorraine@gmail.com>
2017-07-30 16:39:05 -0700

    refactored - final commit

--- databases/kwazy_pwizes/kwazy_pwizes.rb
+++ databases/kwazy_pwizes/kwazy_pwizes.rb
@@ -1,7 +1,16 @@
+#PSUEDOCODE
+# Create a game called Kwazy Pwizes.
+#   Every player wins.
+#   Each turn the database provides a random prize, at a random location presented by a random celebrity.
+#     The Array method 'sample' is used on columns in the database table for randomness.
+#   The user interface keeps the game going.
+#     A player inputs $1 for each turn.
+#     The game only continues IF '$1' is entered.
+#     The output awards the prize to the winning player.
+
 require 'sqlite3'
 
 db = SQLite3::Database.new("kwazy_pwizes.db")
-
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS kwazy_pwizes(
     id INTEGER PRIMARY KEY,
@@ -10,7 +19,6 @@ create_table_cmd = <<-SQL
     celebrity VARCHAR(255)
   )
 SQL
-
 db.execute(create_table_cmd)
 
 # *********DATA POPULATING kwazy_pwizes database************
@@ -25,7 +33,7 @@ db.execute(create_table_cmd)
 # db.execute("INSERT INTO kwazy_pwizes (noun, location, celebrity) VALUES ('dream vacation on Mars', 'Kathmandu Nepal the Himalayan jewel', 'Jimmy Fallon')")
 # db.execute("INSERT INTO kwazy_pwizes (noun, location, celebrity) VALUES ('billion dollars', 'Serengeti National Park in Tanzania', 'Elon Musk')")
 
-
+#use table data as arrays to select random prize information
 nouns = db.execute("SELECT noun FROM kwazy_pwizes")
 prize = nouns.sample
 locations = db.execute("SELECT location FROM kwazy_pwizes")
@@ -34,13 +42,12 @@ celebrities = db.execute("SELECT celebrity FROM kwazy_pwizes")
 star_power = celebrities.sample
 
 # USER INTERFACE
-
-puts "Welcome to KWAZY PWIZES!"
-puts "Enter '$1' to play. Or type 'done' to exit."
-payment = gets.chomp
-# break if payment == 'done'
-if payment == '$1'
-"\n"
-  puts "CONGRATULATIONS!!!****"
-  puts "You won a #{prize}!" "\n" "You will fly to fabulous #{dream_local}." "\n" "And receive your prize from star celebrity #{star_power}!!" "\n" "Hooray!"
-end
+  puts "Welcome to KWAZY PWIZES!"
+  puts "Enter '$1' to play." "\n"
+  payment = gets.chomp
+    if payment == '$1'
+      puts " CONGRATULATIONS!!!****"
+      puts " You won a #{prize}!\n You will fly to fabulous #{dream_local}.
+      \n And receive your prize from star celebrity #{star_power}!!
+      \n Hooray!\n\n\n"
+    end
