69d8434e5ac88fd4590b363d702aa8c34b5a3b39
flash00 <dexlorraine@gmail.com>
2017-08-04 13:03:52 -0700

    Tested form to add new location

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -8,32 +8,35 @@ db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
 # show students on the home page
-get '/' do
-  @students = db.execute("SELECT * FROM students")
-  erb :home
-end
+# get '/' do
+#   @students = db.execute("SELECT * FROM students")
+#   erb :home
+# end
 
+#show campuses on the home page
 get '/' do
   @campuses = db.execute("SELECT distinct(name) FROM campuses")
   erb :DBC_students
 end
 
-get '/' do
-  @students = db.execute("SELECT * FROM students")
-  erb :DBC_students
-end
+# get '/' do
+#   @students = db.execute("SELECT * FROM students")
+#   erb :DBC_students
+# end
 
-get '/students/new' do
-  erb :new_student
-end
+# get '/students/new' do
+#   erb :new_student
+# end
 
-# create new students via
+# # create new students via
+# # a form
+# post '/students' do
+#   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+#   redirect '/'
+# end
+
+# create new campuses via
 # a form
-post '/students' do
-  db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
-  redirect '/'
-end
-
 get '/campuses/new' do
   erb :new_campus_request
 end
