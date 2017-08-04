f32c8950d79844df9c55c68a11bbfa48b3ed45ed
flash00 <dexlorraine@gmail.com>
2017-08-03 17:31:50 -0700

    Added routes to display address, a query parameter if present, and to add 2 numbers

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -5,6 +5,36 @@ require 'sqlite3'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+#write GET route /contact
+#display address
+
+get '/contact' do
+  "<h1><a href='https://www.google.com/maps/place/La+Taqueria/@37.7508961,-122.4202807,17z/data=!3m1!4b1!4m5!3m4!1s0x808f7e46d50136ad:0x21fce96ce541e475!8m2!3d37.7508961!4d-122.4180867?hl=en'>Here is the address for La Taqueria.</h1>"
+end
+
+#write GET route
+#add query parameter
+#add IF statement with ELSE display
+#how to call - localhost:9393/great_job?name=Frank
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+#write GET route
+#add
+
+get '/add/:num1/:num2' do
+  num1 = params[:num1][0].to_i
+  num2 = params[:num2][0].to_i
+  (num1 + num2).to_s
+end
+
 # write a basic GET route
 # add a query parameter
 # GET /
@@ -47,7 +77,4 @@ end
 
 
 
-get '/' do
-  contact = params[:contact]
-  "Here is the address #{contact}."
-end
+
