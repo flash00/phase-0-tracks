7b75ba22feb5e762e2006a30a23804f05f082dcb
flash00 <dexlorraine@gmail.com>
2017-08-03 21:48:37 -0700

    Add Get route that allows user to search database by student age

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -7,16 +7,14 @@ db.results_as_hash = true
 
 #write GET route /contact
 #display address
-
 get '/contact' do
   "<h1><a href='https://www.google.com/maps/place/La+Taqueria/@37.7508961,-122.4202807,17z/data=!3m1!4b1!4m5!3m4!1s0x808f7e46d50136ad:0x21fce96ce541e475!8m2!3d37.7508961!4d-122.4180867?hl=en'>Here is the address for La Taqueria.</h1>"
 end
 
-#write GET route
+#write GET route /great_job
 #add query parameter
 #add IF statement with ELSE display
 #how to call - localhost:9393/great_job?name=Frank
-
 get '/great_job' do
   name = params[:name]
   if name
@@ -26,13 +24,19 @@ get '/great_job' do
   end
 end
 
-#write GET route
-#add
-
+#write GET route /add
+#add route parameters
 get '/add/:num1/:num2' do
   num1 = params[:num1][0].to_i
   num2 = params[:num2][0].to_i
   (num1 + num2).to_s
+end
+
+#write GET route /students/age
+#user searches student information to display by age
+get '/students/:age' do
+  student = db.execute("SELECT * FROM students WHERE age=?", [params[:age]])[0]
+  student.to_s
 end
 
 # write a basic GET route
