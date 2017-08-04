88c0eb7588fdf742b5915f09c50db3ac0456722c
flash00 <dexlorraine@gmail.com>
2017-08-04 12:22:46 -0700

    Add ERB template and HTTP route that responds to it

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -13,6 +13,16 @@ get '/' do
   erb :home
 end
 
+get '/' do
+  @campuses = db.execute("SELECT distinct(name) FROM campuses")
+  erb :DBC_students
+end
+
+get '/' do
+  @students = db.execute("SELECT * FROM students")
+  erb :DBC_students
+end
+
 get '/students/new' do
   erb :new_student
 end
@@ -21,6 +31,15 @@ end
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+  redirect '/'
+end
+
+get '/campuses/new' do
+  erb :new_campus_request
+end
+
+post '/campuses' do
+  db.execute("INSERT INTO campuses (name, nickname) VALUES (?,?)", [params['name'], params['nickname']])
   redirect '/'
 end
 
